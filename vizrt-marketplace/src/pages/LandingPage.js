ffd73ad7a012542b7c16bf8a9e4ce144d02f74a4
oddaks <oddaks96@gmail.com>
2024-06-04 14:49:31 +0200

    add Landing page

---
+++ vizrt-marketplace/src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../components/MarketplaceLogo";
+
+const LandingPage = ()=>{
+    return(
+        <div>
+            <Logo></Logo>
+        </div>
+    )
+}
+export default LandingPage
