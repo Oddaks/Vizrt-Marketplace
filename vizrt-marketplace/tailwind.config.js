5a60210637beaf2e1297037d7ed5bcb98ae5b3eb
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-05 16:42:35 +0200

    Viz-orange added to tailwind config. App.js linked to Footer page. Footer.jsx made

--- vizrt-marketplace/tailwind.config.js
+++ vizrt-marketplace/tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
     extend: {
       backgroundColor: {
         'viz-blue': '#1D363F',
+        'viz-orange': '#EF804E'
       },
       backgroundImage: {
         'gradient-to-r': 'linear-gradient(to right, #D66932, #BC2F14)',
       },
       translate: {
-        '1/8': '12.5%', 
+        '1/8': '12.5%',
         '1/4': '25%',
       }
     },
