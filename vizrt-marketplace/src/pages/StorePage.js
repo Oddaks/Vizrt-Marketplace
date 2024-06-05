e9c014909ee216433a80087b8df2ae056403f435
MarenKristiania <maren.lokoy@gmail.com>
2024-06-05 20:53:13 +0200

    Add Store Page
    Added store page

---
+++ vizrt-marketplace/src/pages/StorePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../components/MarketplaceLogo";
+
+const StorePage = ()=>{
+    return(
+        <div className="bg-viz-blue">
+            
+        </div>
+    )
+}
+export default StorePage
