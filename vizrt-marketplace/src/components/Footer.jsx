c156b193cc8f0488f23d755ee8a8e6d908c066e2
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-05 16:46:14 +0200

    Footer.jsx added

---
+++ vizrt-marketplace/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="relative bg-viz-orange">
+      <div className="flex justify-between space-x-4 items-center text-white">
+        <p className="mb-2 mt-2 p-5 text-left">Trusted By The Best</p>
+        <div className="text-center">
+          <p>Vizrt's Customers Success provides industry-leading service,</p>
+          <p>delivers customer satisfaction,and robust</p>
+          <p>adoption of Vizrt software to best support users.</p>
+        </div>
+        <div className="mb-2 mt-2 p-5 text-right">
+          <p>Our customers tell the worlds stories</p>
+          <p>and we ensure they are told seamlessly</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
