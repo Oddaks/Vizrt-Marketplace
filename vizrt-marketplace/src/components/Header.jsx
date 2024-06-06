4ed5994da5c9429a268e9d2f8230f7e7d3f867e4
hennyej <hennyeline@hotmail.com>
2024-06-06 13:48:26 +0200

    updated

--- vizrt-marketplace/src/components/Header.jsx
+++ vizrt-marketplace/src/components/Header.jsx
@@ -1,50 +1,65 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import MenuLogo from './src/vizrt_logo.png';
 
 const Header = () => {
-  const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
 
-  return (
-    <header className="navbar bg-white text-black p-2 relative">
-      <div className="logo absolute top-2 left-2 w-64 h-24"></div>
-      <nav>
-        <ul className="nav-links list-none m-0 p-0 flex">
-          <li className="arrowback absolute top-32 left-7 w-6 h-5 flex items-start flex-shrink mr-2"></li>
-          <li className="shopping-cart absolute top-0 left-68 w-10 h-10 mr-2">
-            <button className="cart-button">
-              <FontAwesomeIcon icon={faCoffee} />
-            </button>
-          </li>
-          <li className="dropdown mr-2">
-            <button onClick={() => setShowMenu(!showMenu)} className="dropdown-button text-black p-1 no-underline">
-              <FontAwesomeIcon icon={faBars} />
-              {showMenu ? 'X' : 'Menu'}
-            </button>
-            {showMenu && (
-              <div className="dropdown-menu bg-white border border-gray-300 p-2 absolute z-10">
-                <a href="#" className="block p-1">
-                  Button
-                </a>
-                <a href="#" className="block p-1">
-                  About
-                </a>
-                <a href="#" className="block p-1">
-                  Contact
-                </a>
-              </div>
-            )}
-          </li>
-          <li className="searchbar absolute top-0 left-0 w-58 h-10 flex overflow-hidden items-center flex-shrink-0 border border-orange-500 bg-transparent mr-2">
-            <div className="search-bar-container flex items-center">
-              <input type="text" className="search-bar border-none p-1" placeholder="Search..." />
-              <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon ml-1" />
+
+    return (
+        <header className="navbar bg-white text-black p-2 relative w-full h-[100px] flex items-start">
+            <div className="logo absolute top-[10px] left-[8px] w-[270px] h-[90px]">
+                <img src={MenuLogo} alt="Logo" /> 
             </div>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+            <nav className="flex-1">
+                <ul className="nav-links list-none m-0 p-0 flex justify-end items-center">
+                    <li className="mr-2">
+                        <div className="searchbar w-[232px] h-[40px] flex overflow-hidden items-center border border-orange-500 bg-transparent">
+                            <div className="search-bar-container flex items-center w-full">
+                                <input
+                                    type="text"
+                                    className="search-bar border-none p-1 w-full"
+                                    placeholder="Search..."
+                                    onFocus={() => setShowSearch(true)}
+                                    onBlur={() => setShowSearch(false)}
+                                />
+                                {!showSearch && <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon ml-1" />}
+                            </div>
+                        </div>
+                    </li>
+                    <li className="shopping-cart w-10 h-10 mr-2">
+                        <button className="cart-button">
+                            <FontAwesomeIcon icon={faShoppingCart} />
+                        </button>
+                    </li>
+                    <li className="dropdown mr-2 relative">
+                        <button
+                            onClick={() => setShowMenu(!showMenu)}
+                            className="dropdown-button text-black p-1 no-underline"
+                        >
+                            <FontAwesomeIcon icon={faBars} />
+                            {showMenu}
+                        </button>
+                        {showMenu && (
+                            <div className="dropdown-menu bg-[#1A2C33] border border-[#F1834B] p-2 absolute z-10 mt-2">
+                                <a href="#" className="block p-1 text-[#F1834B] border-b border-[#F1834B] last:border-0">
+                                    My profile
+                                </a>
+                                <a href="#" className="block p-1 text-[#F1834B] border-b border-[#F1834B] last:border-0">
+                                    Dashboard
+                                </a>
+                                <a href="#" className="block p-1 text-[#F1834B]">
+                                    Sign Out
+                                </a>
+                            </div>
+                        )}
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
