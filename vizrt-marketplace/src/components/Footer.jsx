a5ef908dcc2dfb174db642d4903866e121d53ef7
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-11 11:37:01 +0200

    Space removed

--- vizrt-marketplace/src/components/Footer.jsx
+++ vizrt-marketplace/src/components/Footer.jsx
@@ -1,18 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="relative bg-viz-orange">
       <div className="flex justify-between space-x-4 items-center text-white">
-        <div className="flex items-center mb-2 mt-2 p-5 text-center"> 
+        <div className="flex items-center mb-2 mt-2 p-5 text-center">
           <FontAwesomeIcon icon={faCircleCheck} className="mr-2" size="2x" />
-          <p className="mx-auto">Trusted By 
-          <br />The Best</p>
+          <p className="mx-auto">Trusted By The Best</p>
         </div>
         <div className="flex items-center">
           <FontAwesomeIcon icon={faCircleCheck} className="mr-2" size="2x" />
-          <div className="text-center text-sm"> 
+          <div className="text-center text-sm">
             <p>
               Vizrt's Customers Success provides industry-leading service,
               <br /> delivers customer satisfaction,
@@ -23,9 +22,10 @@ const Footer = () => {
         </div>
         <div className="flex items-center mb-2 mt-2 p-5">
           <FontAwesomeIcon icon={faCircleCheck} className="mr-2" size="2x" />
-          <div className="text-center"> 
+          <div className="text-center">
             <p>
-              Our customers tell the world's stories <br /> and we ensure they are told seamlessly
+              Our customers tell the world's stories <br /> and we ensure they
+              are told seamlessly
             </p>
           </div>
         </div>
