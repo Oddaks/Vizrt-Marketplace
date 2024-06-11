efb36187185c5b46cb3a2f54d971a012ff2fe216
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-11 18:00:15 +0200

    ProductPage modified

--- vizrt-marketplace/src/pages/ProductPage.js
+++ vizrt-marketplace/src/pages/ProductPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import BackButton from "../components/BackButton";
 import ProductEditor from "../components/ProductEditor";
-import ProductCard from "../components/ProductCard";
+import BundleImages from "../components/BundleImages";
 
 //! Do i need this???
 //  const ProductCard = ({ product }) => {
@@ -20,13 +20,24 @@ const ProductPage = (product) => {
     }
     return (
         <div className="bg-viz-blue">
+            {/* Create space on below back arrow */}
             <div className='p-6'>
                 <BackButton />
                 <ProductEditor />
-
+                <BundleImages />
             </div>
-            <p className="text-white">Hello I am a product</p>
+            {/* <p className="text-white font-bold">Bundle Includes</p> */}
 
+            {/* //! Should the btn be here */}
+            {/* Linking to Checkout Cart page */}
+            {/* <div className="flex justify-end">
+                <Link to="/CartCheckoutPage">
+                    <button className=" bg-viz-orange text-white text-1xl p-4 rounded flex items-center space-x-2 hover:bg-orange-600">
+                        <span>Add to Cart</span>
+                        <FontAwesomeIcon icon={faShoppingCart} />
+                    </button>
+                </Link>
+            </div> */}
         </div>
     )
 }
