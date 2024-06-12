ffb19f72d034d7be6b0a8222d8733795c49ff634
Marie Kogstad <marie.kogstad@hotmail.com>
2024-06-12 23:24:40 +0200

    various updates on edit page
    -filter buttons on store page with filter reset
    -image on productpage
    -layout to match figma
    -color edit functionlity

--- vizrt-marketplace/src/components/ProductEditor.jsx
+++ vizrt-marketplace/src/components/ProductEditor.jsx
@@ -1,58 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faStar, faArrowCircleLeft, faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import BundleImages from "./BundleImages";
 
-const ProductEditor = () => {
-  //! Doing this for now, needs to be replaced with product card
-  const product = {
-    title: "Gamer Bundle",
-    description:
-      "This Gamer bundle includes overlays, banners and other assets for streamers.",
-    image: "https://placehold.co/700x400", // Placeholder image link
+//Adds the color button component
+const ColorButton = ({ color, changeColor, text }) => {
+  return (
+    <button className={`mt-2 text-viz-blue font-bold bg-viz-orange hover:bg-orange-600 w-full py-2`}
+      onClick={() => changeColor(color)}>
+      {text}
+    </button>
+  );
+};
+
+const ProductEditor = ({ product }) => {
+  //Makes a preset of colors you can rotate between. Not responsive code but works for display purposes
+  const [frameColor, setFrameColor] = useState('border-green-500');
+  const colors = ['border-pink-500', 'border-teal-300', 'border-viz-orange', 'border-yellow-300'];
+  const buttonLabels = ["Overlay", "Banners", "Vizrt Orange", "Yellow Delight"];
+
+  //async to make sure the api loads before the code
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+  //handles showing image from api
+  const imageUrl = `/${product.image}`;
+  const { title, description } = product;
+
+  const changeColor = (newColor) => {
+    setFrameColor(newColor);
   };
 
-  const placeholderImages = [
-    "https://placehold.co/300x200",
-    "https://placehold.co/300x200",
-    "https://placehold.co/300x200",
-    "https://placehold.co/300x200",
-  ];
+  //function to change the frame color
+  const cycleColor = (direction) => {
+    const currentIndex = colors.indexOf(frameColor);
+    const nextIndex = (currentIndex + direction + colors.length) % colors.length;
+    setFrameColor(colors[nextIndex]);
+  };
 
   return (
-    <div className="bg-viz-blue min-h-screen p-6 flex">
-      <div className="max-w-7xl mx-auto flex w-full h-full ">
-        {/* Left side with main main image and bundle images */}
-        <div className="w-2/3 pr-4 border-r border-white">
-          {/* Product image placeholder */}
-          <div className="mb-4">
-            <img
-              src={product.image}
-              alt="Product"
-              className="object-cover rounded w-full h-auto"
-            />
+
+    <div >
+      {/*parent div with title*/}
+      <h3 className="col-start-2 col-span-4 text-2xl font-bold text-white">Bundle Includes</h3>
+      <div className="bg-viz-blue min-h-screen p-6 flex">
+        {/* left side of grid - biggest image and arrows*/}
+        <div className="flex-auto grid grid-cols-6 grid-rows-4 gap-4 relative">
+          {/* left arrow cycles negative through array index*/}
+          <button className="absolute text-white left-20 top-1/2 "
+            onClick={() => cycleColor(-1)}>
+            <FontAwesomeIcon icon={faArrowCircleLeft} size="2x" />
+          </button>
+
+          <div className={`col-span-4 col-start-2 row-span-3 ${frameColor} border-4 flex flex-col`}>
+            <img src={imageUrl} alt={title} className="flex-grow" />
           </div>
-          {/* Bundle images */}
-          <div className="text-white font-bold mb-4">
-            <p className="mt-4 p-3">Bundle Includes</p>
-            <div className="flex space-x-4">
-              <BundleImages
-                images={placeholderImages}
-                className="flex flex-col items-center"
-              />
-            </div>
+          {/* right arrow cycles positive through array index*/}
+
+          <button className="absolute text-white right-20 top-1/2 "
+            onClick={() => cycleColor(1)}>
+            <FontAwesomeIcon icon={faArrowCircleRight} size="2x" />
+          </button>
+
+          {/* Parent wrapper for the four smaller divs under the main img*/}
+          <div className="col-span-4 col-start-2 row-start-4 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-3 place-items-start">
+            {/* button on divs linked to color index*/}
+            {colors.map((color, index) => (
+              <div key={index} className={`${color} p-1 flex flex-col border-2 ${color}`}>
+                <img src={imageUrl} alt={title} className="w-full h-auto" />
+                <ColorButton color={color} changeColor={changeColor} text={buttonLabels[index]} />
+              </div>
+            ))}
           </div>
         </div>
-        {/* Right side with vertical line and 3 sections */}
-        <div className="w-1/3 pl-4 flex flex-col justify-between">
+        {/* Right side with product details */}
+        <div className="bg-gray-600 w-1/4 pl-4 flex flex-col justify-between">
           <div className="flex-1 p-4 border-b border-white">
-            <p className="text-white font-bold"> Gamer Bundle</p>
-            <p className="text-white">
-              This Gamer bundle it is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum has been the industry's standard
-              dummy text ever since the 1500s, when an unknown printer took a
-            </p>
+            <p className="text-white font-bold">{title}</p>
+            <p className="text-white">{description}</p>
             <span className="text-white text-xs font-bold">
               Reviews:
               <FontAwesomeIcon icon={faStar} />
@@ -66,10 +91,10 @@ const ProductEditor = () => {
             <p className="text-white">Select a color sample</p>
           </div>
 
-          {/* Linking to Checkout Cart page */}
+          {/* Checkout Cart link */}
           <div className="flex justify-center mt-4">
             <Link to="/CartCheckoutPage">
-              <button className=" bg-viz-orange text-white text-1xl p-4 rounded flex items-center space-x-2 hover:bg-orange-600">
+              <button className="bg-viz-orange text-white text-1xl p-4 rounded flex items-center space-x-2 hover:bg-orange-600">
                 <span>Add to Cart</span>
                 <FontAwesomeIcon icon={faShoppingCart} />
               </button>
@@ -80,4 +105,5 @@ const ProductEditor = () => {
     </div>
   );
 };
+
 export default ProductEditor;
