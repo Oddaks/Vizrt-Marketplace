5023f2e23d928c85f3831e4964baa0e549acae8d
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-12 11:59:38 +0200

    Cleaned up code, and structured the bundle elements

--- vizrt-marketplace/src/components/BundleImages.jsx
+++ vizrt-marketplace/src/components/BundleImages.jsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const BundleImages = ({ images = [] }) => {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex flex-wrap justify-between items-center">
       {images.map((image, index) => (
-        <div key={index} className="w-1/2 p-2">
+        <div key={index} className="w-1/4 p-2 flex flex-col items-center">
           <img
             src={image}
             alt={`Bundle item ${index + 1}`}
-            className="w-full rounded"
+            className="w-full"
           ></img>
+          {/* Description below the image */}
+          <div className=" bg-viz-orange py-2 mt-3 px-10">
+            <p className="text-xs  text-white text-center">
+              Image {index + 1}: Edit Here
+            </p>
+          </div>
         </div>
       ))}
     </div>
