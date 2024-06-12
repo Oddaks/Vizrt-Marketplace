06d9db66ef7790312c072664cab975cf662c25cf
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-12 13:01:37 +0200

    Update ProductPage.js

--- vizrt-marketplace/src/pages/ProductPage.js
+++ vizrt-marketplace/src/pages/ProductPage.js
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import BackButton from "../components/BackButton";
-import ProductEditor from "../components/ProductEditor";
-import BundleImages from "../components/BundleImages";
+import { useParams } from 'react-router-dom';
+import ProductInfo from '../components/ProductInfo';
+import productsArray from '../Tools/ProductArray.json';
 
-const ProductPage = (product) => {
-    const [selectedProduct, setSelectedProduct] = useState(product[0]);
+const ProductPage = () => {
+    const { productId } = useParams();
+    const [product, setProduct] = useState(null);
 
-    const handleProductClick = (product) => {
-        setSelectedProduct(product)
-    }
+    useEffect(() => {
+        const fetchProduct = () => {
+            const productData = productsArray.find(p => p.id === parseInt(productId));
+            setProduct(productData);
+        };
+
+        fetchProduct();
+    }, [productId]);
+
     return (
         <div className="min-h-screen bg-viz-green">
-            <div className="bg-viz-blue">
-                {/* Create space on below back arrow */}
-                <div className='p-6'>
-                    <BackButton />
-                    <ProductEditor />
-                    <BundleImages />
-                </div>
+            <div className='p-6'>
+                <BackButton />
             </div>
-            );
+            <div className="grid grid-cols-1 p-4">
+                <ProductInfo product={product} />
+            </div>
+        </div>
+    );
 };
 
-            export default ProductPage;
-
-            )
-}
-            export default ProductPage;
+export default ProductPage;
