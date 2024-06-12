94d978347cfe9afd5fe14ebaa08af0c0e5da2e1f
Shir-oh <90028433+Shir-oh@users.noreply.github.com>
2024-06-12 12:13:04 +0200

    Deleted unnecessary  comments

--- vizrt-marketplace/src/pages/ProductPage.js
+++ vizrt-marketplace/src/pages/ProductPage.js
@@ -3,15 +3,6 @@ import BackButton from "../components/BackButton";
 import ProductEditor from "../components/ProductEditor";
 import BundleImages from "../components/BundleImages";
 
-//! Do i need this???
-//  const ProductCard = ({ product }) => {
-//      const { addToCart } = useCart();
-//      const handleAddToCart = () => {
-//          addToCart(product);
-//     };
-//  };
-
-
 const ProductPage = (product) => {
     const [selectedProduct, setSelectedProduct] = useState(product[0]);
 
@@ -26,19 +17,7 @@ const ProductPage = (product) => {
                 <ProductEditor />
                 <BundleImages />
             </div>
-            {/* <p className="text-white font-bold">Bundle Includes</p> */}
-
-            {/* //! Should the btn be here */}
-            {/* Linking to Checkout Cart page */}
-            {/* <div className="flex justify-end">
-                <Link to="/CartCheckoutPage">
-                    <button className=" bg-viz-orange text-white text-1xl p-4 rounded flex items-center space-x-2 hover:bg-orange-600">
-                        <span>Add to Cart</span>
-                        <FontAwesomeIcon icon={faShoppingCart} />
-                    </button>
-                </Link>
-            </div> */}
         </div>
     )
 }
-export default ProductPage
+export default ProductPage;
